74e5f7191f3b1842d208a8a4bc94c9b006b1d00f
maxano <maxano@gmail.com>
2018-01-18 14:13:03 +0300

    Optimized a sitemap generation.

--- sitemap.py
+++ sitemap.py
@@ -10,20 +10,23 @@ class SCMSSitemap(Sitemap):
 
     def items(self):
         self.default_language = get_default_language()
-        qs = Page.objects.filter(published=1, slugs__language__in=[lang[0] for lang in settings.SCMS_LANGUAGES]).order_by('id','slugs__alias').extra(select={
+        # qs = Page.objects.filter(published=1, slugs__language__in=[lang[0] for lang in settings.SCMS_LANGUAGES]).order_by('id','slugs__alias').extra(select={
+        #         'alias': 'IF (`scms_page`.`state` = %s, "/", REPLACE(`scms_slugs`.`alias`,"*",""))' % page_state.MAIN,
+        #         'language': '`scms_slugs`.`language`',
+        #         }).exclude(state__in=[page_state.EXTRAHIDDEN, page_state.IN_TRASH, page_state.SETTINGS]).exclude(slugs__alias=None)
+        qs = Slugs.objects.filter(page__published=1, language__in=[lang[0] for lang in settings.SCMS_LANGUAGES]).order_by('id','alias').extra(select={
                 'alias': 'IF (`scms_page`.`state` = %s, "/", REPLACE(`scms_slugs`.`alias`,"*",""))' % page_state.MAIN,
                 'language': '`scms_slugs`.`language`',
-                }).exclude(state__in=[page_state.EXTRAHIDDEN, page_state.IN_TRASH, page_state.SETTINGS]).exclude(slugs__alias=None)
-        len(qs) # Почему то по разному считается len(qs) и qs.count. И коунт считается не правильно из Sitemap.paginator. А если мы тут делаем len, то считается всё правильно. Google: django count and len different
+                }).exclude(page__state__in=[page_state.EXTRAHIDDEN, page_state.IN_TRASH, page_state.SETTINGS]).exclude(alias=None).values("page__modifed", "language", "page__date", "id", "alias")
         return qs
 
     def lastmod(self, obj):
-        return  obj.modifed and obj.modifed or obj.date
+        return  obj.get("page__modifed") or obj.get("page__date")
     
     def location(self, obj):
-        if obj.alias == None: # Нарушена целосность БД и есть страницы, для которых нет Slug
+        if obj.get("alias") == None: # Нарушена целосность БД и есть страницы, для которых нет Slug
             return '/' 
-        if obj.language == self.default_language or obj.language is None:
-            return obj.alias
+        if obj.get("language") == self.default_language or obj.get("language") is None:
+            return obj.get("alias")
         else:
-            return '/%s%s' % (obj.language, obj.alias)
+            return '/%s%s' % (obj.get("language"), obj.get("alias"))
